d8a888dec66dfb3f72c92e43fe60aef2b325ec93
soyunandres <53534419+soyunandres@users.noreply.github.com>
2022-10-22 16:55:53 -0500

    wip(ml)bi/analisis-tecnico/tipo-de-cambio: Crear archivo de historia de usuario
    Se genera archivo de historia de usuario con sintaxis Gherkin

---
+++ Test/AnalisisTecnicoTipoDeCambioUserStory.R
@@ -0,0 +1,14 @@
+# Necesidad del negocio:
+    # Como analista 
+    # Necesito obtener un análisis del tipo de cambio 
+    # De modo que pueda compartirlo como PDF
+
+# Reglas de negocio:
+
+# Característica:
+    # Dado que instalo las librerías que necesito 
+    install.packages("box")
+
+
+    # Cuando las invoco
+    # Entonces puedo utilizarlas 
